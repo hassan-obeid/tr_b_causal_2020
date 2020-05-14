a0132f13fbe87a1b48ce3807b3cd05dfc512dd5e
Timothy Brathwaite <timothyb0912@gmail.com>
2020-05-14 13:39:39 -0700

    Moving causal graph definition to src directory and adding description of latent confounding issues.

--- notebooks/working/_04-tb-parking-choice-example.py
+++ notebooks/working/_04-tb-parking-choice-example.py
@@ -18,99 +18,53 @@
 # The study of note is
 # > Chiara, Giacomo Dalla, Lynette Cheah, Carlos Lima Azevedo, and Moshe E. Ben-Akiva. "A Policy-Sensitive Model of Parking Choice for Commercial Vehicles in Urban Areas." Transportation Science (2020).
 #
-# This notebook draw the causal diagram that we believes corresponds to the parking choice model in that paper.
+# In this notebook we draw the causal diagram that we believes corresponds to the parking choice model in Chiara et al (2020).
 # We then point out some tests that could have been done and should be presented to justify this paper's causal model and its implicit assumptions, even before estimating parameters of the proposed choice models.
 # Finally, we will:
 # - note the importance of these implicit assumptions, and
-# - show this study may present a real example of our presentations simplified demsonstration of how incorrect causal graphs can lead to highly inaccurate causal effect estimates, even in selection-on-observables settings.
+# - show how this study may present a real example of our simplified demsonstration of how incorrect causal graphs can lead to highly inaccurate causal effect estimates, even in selection-on-observables settings.
 #
-
-from causalgraphicalmodels import CausalGraphicalModel
 
 # +
-# Note that string representations stored
-# as objects so I can later easily change the
-# string representations in one place and update
-# the representation everywhere
-parking_duration = 'parking_duration\n(d_n)'
-vehicle_type = 'vehicle_type\n(v_n)'
-choice_set = 'choice_set\n(C_n)'
-parking_tariffs = 'parking_tariffs'
-parking_fines = 'parking_fines'
-parking_cost = 'parking_cost\n(C_ni)'
-vehicle_attributes = 'vehicle_attributes\n(Z_n)'
-parking_congestion = 'parking_congestion\n(q_it)'
-parking_capacity = 'parking_capacity\n(N_i)'
-explanatory_features = 'explanatory_features\n(X_nit)'
-unobserved_confounders = 'unobserved_confounders'
-utility = 'utility\n(U_nit)'
-availability = 'availability\n(A_nit)'
-choice = 'choice\n(Y_nit)'
+import sys
 
-nodes =\
-    [parking_duration,
-     vehicle_type,
-     choice_set,
-     parking_tariffs,
-     parking_fines,
-     parking_cost,
-     vehicle_attributes,
-     parking_congestion,
-     parking_capacity,
-     explanatory_features,
-     utility,
-     availability,
-     choice,
-    ]
-
-edges =\
-    [(parking_duration, parking_cost),
-     (vehicle_type, parking_cost),
-     (parking_tariffs, parking_cost),
-     (parking_fines, parking_cost),
-     (parking_duration, parking_fines),
-     (vehicle_type, choice_set),
-     (choice_set, availability),
-     (availability, choice),
-     (parking_cost, explanatory_features),
-     (explanatory_features, utility),
-     (utility, choice),
-     (vehicle_attributes, explanatory_features),
-     (parking_congestion, explanatory_features),
-     (parking_capacity, parking_congestion),
-    ]
-
-latent_edges = [(parking_congestion, utility)]
-
-parking_causal_model =\
-    CausalGraphicalModel(
-        nodes=nodes,
-        edges=edges,
-        latent_edges=latent_edges
-    )
-
-parking_causal_model.draw()
+sys.path.insert(0, '../../src/')
+from graphs import parking
 # -
 
-# As usual, one immediate question is whether this causal model's testable implications are supported by the author's dataset.
+causal_graph = parking.PARKING_CAUSAL_MODEL.draw()
+causal_graph.graph_attr.update(size="10,6")
+causal_graph
+
+# # Issues
+# As usual, the immediate and general question is whether this causal model is credible.
+# That is,
+# 1. Are the model's testable implications are supported by the author's dataset?
+# 2. Are the untestable assumptions plausible?
 #
-# More specific questions arise when one considers the models and policy interventions analyzed in the paper.
+# These two issues will be expounded on below.
+#
+# ## 1. Structure of the observables
+# As good practice, one should test each and every testable implication of one's model.
+#
+# However, the implications / assumptions related to one's study's interventions are likely to be most important.
 # In particular, the authors consider policy interventions that set or alter:
 # - parking capacity
 # - parking duration
 # - parking tariffs
 # - parking fines
 #
-# One is led to immediately wonder how parking duration relates to parking tariffs and vehicle attributes such as the type of vehicle owner (e.g. large corporation vs independent truck owner) and the type of goods being transported (large heavy loads vs small loads).
-# Additionally, it might be reasonable to expect that there may be unobserved confounders that lead to loading or unloading delays for all drivers at a particular parking facility, thus affecting both parking congestion and parking duration.
-# Finally, it makes sense that at locations with higher parking tariffs, that driver's and workers may have greater incentive to minimize parking duration to avoid higher costs.
+# One is led to immediately wonder
+#    1. how might parking duration relate to parking tariffs and vehicle attributes such as the type of vehicle owner (e.g. large corporation vs independent truck owner) and the type of goods being transported (large heavy loads vs small loads)?
+#    2. are there may be unobserved confounders that lead to loading or unloading delays for all drivers at a particular parking facility (e.g. a closed ramp), thus affecting both parking congestion and parking duration?
+#    3. at locations with higher parking tariffs, will drivers and workers experience greater incentive to minimize parking duration to avoid higher costs?
 #
 # Chiara et al., state that they tread parking duration as "exogenous and known by the driver before making a parking choice" (p. 7).
-# Given the concerns in the preceding paragraph, the assumption that parking duration is exogeneous is a-priori suspect.
-# The causal graph above suggests testing this assumption by attempting to falsify the implicit assumptions of:
-# - marginal independence between vehicle attributes and parking duration
-# - marginal independence between parking duration and parking tariffs
-# - marginal independence between parking duration and parking congestion.
+# **Given the concerns in the preceding paragraph, assuming that parking duration is exogeneous is a-priori suspect.**
+# The causal graph above suggests testing this assumption by attempting to falsify the implicit assumptions of marginal independence between parking duration and:
+# - vehicle attributes ($Z_n$)
+# - parking tariffs
+# - parking congestion ($q_{it}$)
 #
 # The exogeneity assumption is crucially important, as noted by Chiara et al.
 # From a statistical perspective, the authors note on page 14 that,
@@ -119,5 +73,29 @@ parking_causal_model.draw()
 #
 # From a causal perspective, the causal effect of the author's considered policy interventions depends on the assumed causal relationships.
 # **If parking duration is partially caused by parking tariffs, then the estimated causal effects of changing parking tariffs may be highly inaccurate because these downstream effects were ignored.**
-# Likewise, if parking duration is partially caused by vehicle attributes such as the type of goods being transported, then the considered interventions that limit parking durations to a given time may be unrealistic (i.e., not useful).
-# There may be physical limits to how low one's parking duration can be, given the type and quantity of goods being delivered or picked-up. 
+# The harmful effects of ignoring downstream changes that occur from intervening on an upstream variable is demonstrated in the first part of our work.
+#
+# Moreover, if parking duration is partially caused by vehicle attributes such as the type of goods being transported, then **the considered interventions that limit parking durations to a given time may be unrealistic (i.e., not useful).**
+# For instance, there may be physical limits to how low one's parking duration can be, given the type and quantity of goods being delivered or picked-up.
+# In such a case, the simulated interventions are unlikely to be feasible in reality.
+#
+# ## 2. Structure of unobservables
+# Chiara et al. are concerned about latent confounding / endogeneity of parking congestion and utility.
+# To deal with this problem, they assume that parking arrivals in the previous time period $(t, t-1h)$ is an instrumental variable.
+# Note that time is presumably being measured in hours.
+#
+# To be explicit, by assuming that parking arrivals in the previous hour is an instrumental variable, Chiara et al. assume that
+# 1. parking arrivals in the previous hour is not caused by the same unobserved confounders that cause parking congestion in the current hour,
+# 2. parking arrivals in the previous hour cause (or are associated with) parking congestion in the current hour,
+# 3. the utility of parking at a given location is independent of parking arrivals in the previous hour, conditional on parking congestion 
+#
+# However, the assumption that parking arrivals in the previous hour is not caused by the same unobserved confounders that cause parking congestion in the current hour is a-priori suspect.
+# Chiara et al. hypothesize that the unobserved confounders include "factors such as a temporary closure of a road lane or the passage of a police car" (p. 14).
+# If for instance, a roadway or lane is closed due to construction, such a closure may last for multiple hours or even days.
+# In such a case, the unobserved confounders would affect not just parking congestion in the current hour, but arrivals to the parking facility in the previous hour as well.
+#
+# Given the situation described in the preceding paragraph, **the parking arrivals in the previous hour do not appear to meet the conditions of an instrumental variable**, thus decreasing or invalidating the credibility of the control function approach used in the article.
+#
+# This issue is an example of the old adage that "good [or valid] instruments are hard to find."
+#
+# More generally, this directly relates to the second point in our work, that **dealing with unobserved confounding in observational studies is an open and difficult problem.**
