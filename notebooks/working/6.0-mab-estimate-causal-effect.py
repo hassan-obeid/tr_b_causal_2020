a0a14de174637b97c29c72a065d2d798ce73c001
Mohamed Amine Bouzaghrane <amine.bouzaghrane@gmail.com>
2020-05-15 13:52:44 -0400

    refactoring as follows:
    - updating some variable names and fixing logic of assigning variable name (alt specific vs ind specific) in notebook 3.
    - refactor the get_dist_node_no_parent function in notebook 6.

--- notebooks/working/6.0-mab-estimate-causal-effect.py
+++ notebooks/working/6.0-mab-estimate-causal-effect.py
@@ -418,21 +418,13 @@ from array import array
 ## Functions to replace code within
 ## DistNodeNoParent
 ## Function for checking length
-def isUnique(var_values):
+def is_unique(var_values):
     """
     Checks whether a variable has one unique value.
     """
     return len(var_values.unique()) == 1
 
-def isConstant(var_type):
-    """
-    Checks whether the variable type for the
-    variable of interest is to be taken
-    as a constant value or as numerical values.
-    """
-    return var_type == 'constant'
-
-def isEmpirical(var_type):
+def is_empirical(var_type):
     """
     Checks whether the variable type for the
     variable of interest is to be taken
@@ -440,44 +432,76 @@ def isEmpirical(var_type):
     """
     return var_type in ['constant','numerical']
 
+def is_empirical(var_type):
+    """
+    Checks whether the variable type for the
+    variable of interest is to be taken
+    as a constant value or as numerical values.
+    """
+    return var_type == 'empirical'
+
 ## Function for checking if variable is categorical
-def isCategorical(var_type):
+def is_categorical(var_type):
     """Checks whether the variable type for the
     variable of interest is categorical.
     """
     return var_type == 'categorical'
 
-def getConstantDist(ind_var, var_val):
-    """
-    Retrives the constant 'distribution' of a constant
-    variable.
-    """
-    constant_dict = {}
-    constant_dict.setdefault(ind_var,{})['distribution'] = 'constant'
-    constant_dict.setdefault(ind_var,{})['parameters'] = var_val.unique()
-    return constant
+def get_alt_specific_variable_name(var_name, alt_name):
+    return var_name + '_' + alt_name
 
-def getCategoricalDist(ind_var, var_val):
+def get_constant_dist(var, var_val, alt_name=None):
+    """
+    Retrives the constant 'distribution' of a 
+    constant variable.
+    """
+    constant_dict = defaultdict(dict)
+    # Add name of alternative to variable and store distriburion & parameters
+    var_name =\
+        var if alt_name is None else get_alt_specific_variable_name(var, alt_name)
+    constant_dict[var_name]['distribution'] = 'constant'
+    constant_dict[var_name]['parameters'] = var_val.unique()
+    return constant_dict
+
+def get_empirical_dist(var, var_val, alt_name=None):
+    """
+    Retrives the empirical values of the alternative
+    specific variable of interest as its distribution.
+    """
+    empir_dict = defaultdict(dict)
+    # Add name of alternative to variable and store distriburion & parameters
+    var_name =\
+        var if alt_name is None else get_alt_specific_variable_name(var, alt_name)
+    empir_dict[var_name]['distribution'] = 'empirical'
+    empir_dict[var_name]['parameters'] = np.array(var_val)
+    return empir_dict
+
+def get_categorical_dist(var, var_val, alt_name=None):
     """
     Retrives the unique values and the proportions
-    of observed values for a categorical variable.
+    of observed values for a categorical alternative
+    specific variables.
     """
-    categ_dict = {}
-    categ_dict.setdefault(ind_var,{})['distribution'] = 'categorical'
-    # Count frequency of values and store it as paramater of distribution
-    np_array_range = np.arange(var_val.max()+1)
+    categ_dict = defaultdict(dict)
+    # If more than one category, compute the frequency of values
+    # and store as parameters
+    # Add name of alternative to variable and store distriburion & parameters
+    var_name =\
+        var if alt_name is None else get_alt_specific_variable_name(var, alt_name)
+    categ_dict[var_name]['distribution'] = 'categorical'
+    np_array_range = np.arange(var_val.max()+1) #var_val = alt_data[alt_var]
     array_bincount = np.bincount(var_val)
     probs = array_bincount / len(var_val)
-    categ_dict.setdefault(ind_var,{})['parameters'] = [np_array_range,
-                                      probs]
+    categ_dict[var_name]['parameters'] = [np_array_range,
+                                          probs]
     return categ_dict
 
-def getContinuousDist(ind_var, var_val, cont_dists):
+def get_continuous_dist(var, var_val, alt_name=None, cont_dists): #alt_name_dic[alt]
     """
-    Retrives the distribution of continuous variables
-    using the Fitter package.
+    Retrives the distribution of continuous alternative
+    specific variables using the Fitter package.
     """
-    cont_dict = {}
+    cont_dict = defaultdict(dict)
     # Use the Fitter library to fit distributions
     # to the data
     fitter_object = Fitter(data=var_val,
@@ -487,12 +511,49 @@ def getContinuousDist(ind_var, var_val, cont_dists):
     fitter_object.fit()
     # Get the best distribution and store in dictionary
     BestDict = fitter_object.get_best()
-    cont_dict.setdefault(ind_var,{})['distribution'] = list(BestDict.items())[0][0]
-    cont_dict.setdefault(ind_var,{})['parameters'] = list(BestDict.items())[0][1]
+    # Add name of alternative to variable and store distriburion & parameters
+    var_name =\
+        var if alt_name is None else get_alt_specific_variable_name(var, alt_name)
+    cont_dict[var_name]['distribution'] =\
+        list(BestDict.items())[0][0]
+    cont_dict[var_name]['parameters'] =\
+        list(BestDict.items())[0][1]
     return cont_dict
-    
 
-def indSpecDist(data_long, obs_id_col, ind_spec, var_types, cont_dists):
+def get_distribution_dicts(var, var_type, var_val, alt_name=None, cont_dists):
+    # If data is categorical
+    if is_empirical(var_type):
+        # If only one category
+        if is_unique(var_val):
+            # Add name of alternative to variable
+            # and store distriburion & parameters
+            dist_dic = get_constant_dist(var, var_val, alt_name)
+        else:
+            dist_dic = get_empirical_dist(var, var_val, alt_name)
+    elif is_categorical(var_type):
+        if is_unique(var_val):
+            dist_dic = get_constant_dist(var, var_val, alt_name)
+        else:
+            dist_dic = get_categorical_dist(var, var_val, alt_name)
+    else:
+        # If data is not categorical but has one unique value
+        if is_unique(var_val):
+            dist_dic = get_constant_dist(var, var_val, alt_name)
+        # If data is not categorical but has more than one unique value
+        else:
+            dist_dic = get_continuous_dist(var, var_val, alt_name, cont_dists)
+    return dist_dic
+
+
+############################################
+############################################
+## Functions to replace functionality for
+## fitting distributions for variables
+## specific variables that have no parents
+## in the causal graph.
+##############################
+
+def ind_spec_dist(data_long, obs_id_col, ind_spec, var_types, cont_dists):
     """
     Function that retrieves distributions for all individual
     specific variables.
@@ -500,203 +561,86 @@ def indSpecDist(data_long, obs_id_col, ind_spec, var_types, cont_dists):
     ind_spec_dict = {}
     for ind_var in ind_spec:
         # generate array of values for individual specific variable
-        var_val = pd.Series([(data_long.loc[data_long[obs_id_col] == x][ind_var].unique()[0]) for x in data_long[obs_id_col].unique()])  
-        # Get distribution if variable is categorical
+        var_val =\
+            (data_long[[obs_id_col, ind_var]]
+                .drop_duplicates(obs_id_col, inplace=False)
+                .loc[:, ind_var]
+                .reset_index(drop=True)
+            )  
+        # Get distribution of variable
         var_type = var_types[ind_var]
-        if isCategorical(var_type):
-            # If only one category
-            if isUnique(var_val):
-                ind_var_dict = getConstantDist(ind_var, var_val)
-            # If more than one category
-            else:
-                ind_var_dict = getCategoricalDist(ind_var, var_val)
-        else:
-            # If not categorical but just one unique value
-            if isUnique(var_val):
-                ind_var_dict = getConstantDist(ind_var, var_val)
-            # If not categorical but not one unique value
-            else:
-                ind_var_dict = getContinuousDist(ind_var, var_val, cont_dists)
+        ind_var_dic =\
+            get_distribution_dicts(ind_var,
+                                   var_type,
+                                   var_val,
+                                   cont_dists)
         ind_spec_dict.update(ind_var_dict)
     return ind_spec_dict
 
-##############################################################
-##############################################################
-## Functions to replace functionality for
-## fitting distributions for alternative
-## specific variables that have no parents
-## in the causal graph.
-
-def isEmpirical(var_type):
-    """
-    Checks whether the variable type for the
-    variable of interest is to be taken
-    as a constant value or as numerical values.
-    """
-    return var_type == 'empirical'
-
-def getConstantDistAlt(alt_var, alt_data, alt_name_dic, alt):
-    """
-    Retrives the constant 'distribution' of a 
-    constant variable.
-    """
-    constant_dict = {}
-    # Add name of alternative to variable and store distriburion & parameters
-    constant_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['distribution'] = 'constant'
-    constant_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['parameters'] = alt_data[alt_var].unique()
-    return constant_dict
-
-def getEmpiricalDistAlt(alt_var, alt_data, alt_name_dic, alt):
-    """
-    Retrives the empirical values of the alternative
-    specific variable of interest as its distribution.
-    """
-    empir_dict = {}
-    # Add name of alternative to variable and store distriburion & parameters
-    empir_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['distribution'] = 'empirical'
-    empir_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['parameters'] = np.array(alt_data[alt_var])
-    return empir_dict
-
-def getCategoricalDistAlt(alt_var, alt_data, alt_name_dic, alt):
-    """
-    Retrives the unique values and the proportions
-    of observed values for a categorical alternative
-    specific variables.
-    """
-    categ_dict = {}
-    # If more than one category, compute the frequency of values
-    # and store as parameters
-    # Add name of alternative to variable and store distriburion & parameters
-    categ_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['distribution'] = 'categorical'
-    np_array_range = np.arange(alt_data[alt_var].max()+1)
-    array_bincount = np.bincount(alt_data[alt_var])
-    probs = array_bincount / len(alt_data[alt_var])
-    categ_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['parameters'] = [np_array_range,
-                                                                            probs]
-    return categ_dict
-
-
-def getContinuousDistAlt(alt_var, alt_data, alt_name_dic, alt, cont_dists):
-    """
-    Retrives the distribution of continuous alternative
-    specific variables using the Fitter package.
-    """
-    cont_dict = {}
-    # Use the Fitter library to fit distributions
-    # to the data
-    fitter_object = Fitter(data=alt_data[alt_var],
-                           distributions=cont_dists,
-                           timeout=60,
-                           verbose=False)
-    fitter_object.fit()
-    # Get the best distribution and store in dictionary
-    BestDict = fitter_object.get_best()
-    # Add name of alternative to variable and store distriburion & parameters
-    cont_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['distribution'] = list(BestDict.items())[0][0]
-    cont_dict.setdefault(alt_var +'_'+ alt_name_dic[alt], {})['parameters'] = list(BestDict.items())[0][1]
-    return cont_dict
-##############################
-
-def altSpecDist(data_long, alt_id_col, alt_spec_dic, var_types, alt_name_dic, cont_dists):
+def alt_spec_dist(data_long, alt_id_col, alt_spec_dic, var_types, alt_name_dic, cont_dists):
     """
     Function that retrieves distributions for all alternative
     specific variables.
     """
-    all_alt_spec_var_dic = {}
-    for alt in data_long[alt_id_col].unique():
+    all_alt_spec_var_dic = defaultdict(dict)
+    for alt_id in data_long[alt_id_col].unique():
         # Store data for specific alternative (mode)
-        alt_data = data_long.loc[data_long[alt_id_col] == alt]
-        alt_spec_var_dic = {}
-        # Loop around the alternative specific variables in the input dictionary
-        for alt_var in alt_spec_dic[alt]:
+        alt_data = data_long.loc[data_long[alt_id_col] == alt_id]
+        alt_spec_var_dic = defaultdict(dict)
+        # Loop around the alternative specific
+        # variables in the input dictionary
+        alt_name = alt_name_dic[alt_id]
+        for alt_var in alt_spec_dic[alt_id]:
+            var_val = alt_data[alt_var]
             var_type = var_types[alt_var]
-            # If data is to be taken as empirical values
-            if isEmpirical(var_type):
-                # If only one value
-                if isUnique(alt_data[alt_var]):
-                    alt_spec_var_dic = getConstantDistAlt(alt_var, alt_data, alt_name_dic, alt)
-                else:
-                    alt_spec_var_dic = getEmpiricalDistAlt(alt_var, alt_data, alt_name_dic, alt)
-            # If data is categorical
-            elif isCategorical(var_type):
-                # If only one category
-                if isUnique(alt_data[alt_var]):
-                    alt_spec_var_dic = getConstantDistAlt(alt_var, alt_data, alt_name_dic, alt)
-                else:
-                    alt_spec_var_dic = getCategoricalDistAlt(alt_var, alt_data, alt_name_dic, alt)
-            else:
-                # If data is not categorical but has one unique value
-                if isUnique(alt_data[alt_var]):
-                    # Add name of alternative to variable and store distriburion & parameters
-                    alt_spec_var_dic = getConstantDistAlt(alt_var, alt_data, alt_name_dic, alt)
-                # If data is not categorical but has more than one unique value
-                else:
-                    alt_spec_var_dic = getContinuousDistAlt(alt_var, alt_data, alt_name_dic, alt, cont_dists)
-            alt_spec_var_dic.update(alt_spec_var_dic)
+            alt_spec_var_dist =\
+                get_distribution_dicts(alt_var,
+                                       var_type,
+                                       var_val,
+                                       alt_name,
+                                       cont_dists)
+            alt_spec_var_dic.update(alt_spec_var_dist)
         all_alt_spec_var_dic.update(alt_spec_var_dic)
     return all_alt_spec_var_dic
 
 
-###########################################
-def getEmpiricalDist(trip_var, trip_var_data):
-    """
-    Retrives the empirical values of the trip
-    specific variable of interest as its distribution.
-    """
-    empir_dict = {}
-    empir_dict.setdefault(trip_var, {})['distribution'] = 'empirical'
-    empir_dict.setdefault(trip_var, {})['parameters'] = np.array(trip_var_data)
-    return empir_dict
-
-    
-def tripSpecDist(data_long, obs_id_col, trip_spec, var_types, cont_dists):
+def trip_spec_dist(data_long, obs_id_col, trip_spec, var_types, cont_dists):
     """
     Function that retrieves distributions for all trip
     specific variables.
-    """    # Trip Specific Variable (maybe combine with individual specific variables)
+    """ 
+    # Trip Specific Variable (maybe combine with individual specific variables)
     # Loop around trip (observation) specific variables
-    trip_spec_dict = {}
+    trip_spec_dict = defaultdict(dict)
     for trip_var in trip_spec:
         # generate array of values for trip specific variable
-        trip_var_data = pd.Series([(data_long.loc[data_long[obs_id_col] == x][trip_var].unique()[0]) for x in data_long[obs_id_col].unique()])
+        var_val =\
+            (data_long[[obs_id_col, trip_var]]
+                .drop_duplicates(obs_id_col, inplace=False)
+                .loc[:, trip_var]
+                .reset_index(drop=True)
+            )
         var_type = var_types[trip_var]
         # If data is to be taken as empirical values
-        if isEmpirical(var_type):
-            # If data has one unique value
-            if isUnique(trip_var_data):
-                trip_spec_var_dic = getConstantDist(trip_var, trip_var_data)
-            # If data has more than one unique value
-            else:
-                trip_spec_var_dic = getEmpiricalDist(trip_var, trip_var_data)
-        # Get distribution if variable is categorical
-        elif isCategorical(var_type):
-            # If only one category
-            if isUnique(trip_var_data):
-                trip_spec_var_dic = getConstantDist(trip_var, trip_var_data)
-            # If more than one category
-            else:
-                trip_spec_var_dic = getCategoricalDist(trip_var, trip_var_data)
-        else:
-            # If not categorical but just one unique value
-            if isUnique(trip_var_data):
-                trip_spec_var_dic = getConstantDist(trip_var, trip_var_data)
-            # If not categorical but just one unique value
-            else:
-                trip_spec_var_dic = getConstantDist(trip_var, trip_var_data, cont_dists)
-        trip_spec_dict.update(trip_spec_var_dic)
+        trip_spec_var_dist =\
+            get_distribution_dicts(trip_var,
+                                   var_type,
+                                   var_val,
+                                   cont_dists)
+        trip_spec_dict.update(trip_spec_var_dist)
     return trip_spec_dict
 
 
 ## Define the main function
-def DistNodeNoParent(data_long,
-                     alt_id_col,
-                     obs_id_col,
-                     alt_spec_dic,
-                     alt_name_dic,
-                     ind_spec,
-                     trip_spec,
-                     var_types,
-                     cont_dists=None):
+def get_dist_node_no_parent(data_long,
+                            alt_id_col,
+                            obs_id_col,
+                            alt_spec_dic,
+                            alt_name_dic,
+                            ind_spec,
+                            trip_spec,
+                            var_types,
+                            cont_dists=None):
     """
     Function to find the distribution of specific variables
     from a long format dataset.
@@ -750,17 +694,17 @@ def DistNodeNoParent(data_long,
     params_dict = {}
     
     # Code for Individual Specific Variables
-    ind_spec_dic_params = indSpecDist(data_long, obs_id_col, ind_spec, var_types, cont_dists)
+    ind_spec_dic_params = ind_spec_dist(data_long, obs_id_col, ind_spec, var_types, cont_dists)
     params_dict.update(ind_spec_dic_params)
 
     # Code for Alternative Specific Variables
     # Loop around the different available alternatives
-    alt_spec_dic_params = altSpecDist(data_long, alt_id_col, alt_spec_dic, var_types, alt_name_dic, cont_dists) 
+    alt_spec_dic_params = alt_spec_dist(data_long, alt_id_col, alt_spec_dic, var_types, alt_name_dic, cont_dists) 
     params_dict.update(alt_spec_dic_params)
     
     # Trip Specific Variable (maybe combine with individual specific variables)
     # Loop around trip (observation) specific variables
-    trip_spec_dic_params = tripSpecDist(data_long, obs_id_col, trip_spec, var_types, cont_dists)
+    trip_spec_dic_params = trip_spec_dist(data_long, obs_id_col, trip_spec, var_types, cont_dists)
     params_dict.update(trip_spec_dic_params)
     
     return params_dict
@@ -2680,7 +2624,7 @@ class TestSuite(unittest.TestCase):
         ratio = expected_sum/actual_sum
         difference = abs(ratio-1)
         np.testing.assert_array_less(difference, 0.05) # 0.05 can be discussed
-        
+
 
 # %%
 if __name__ == '__main__':
