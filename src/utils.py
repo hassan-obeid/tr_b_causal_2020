8bd6f41ef88e0eb8e4e3df050b8240e32b7083e7
Timothy Brathwaite <timothyb0912@gmail.com>
2020-05-30 23:37:22 -0700

    [WIP] First draft of factor model sampling function.

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ Generic utilities that helpful across project-submodules.
 """
 from pathlib import Path
 
+from scipy.stats.distributions import rv_generic
 from causalgraphicalmodels import CausalGraphicalModel
 
 PROJECT_ROOT = Path(__file__).parent.parent
@@ -53,3 +54,33 @@ def create_graph_image(
         format=output_type
     )
     return None
+
+
+def sample_from_factor_model(loadings_dist: rv_generic,
+                             coef_dist: rv_generic,
+                             noise_dist: rv_generic,
+                             standard_deviations: np.ndarray,
+                             means: np.ndarray,
+                             num_obs: int,
+                             num_predictors: int,
+                             num_samples: int,
+                             num_factors: int=1) -> np.ndarray:
+    # Get samples of loadings, coefficients, and noise terms
+    coef_samples =\
+        coef_dist.rvs((num_factors, num_predictors, num_samples))
+    loadings_samples =\
+        loadings_dist.rvs((num_obs, num_factors, num_samples))
+    noise_samples =\
+        noise_dist.rvs(size=(num_obs,
+                             num_predictors,
+                             num_samples))
+    # Combine the samples according to the probabilistic factor model
+    x_standardized_samples =\
+        (np.einsum('mnr,ndr->mdr', loadings_samples, coef_samples) +
+         noise_samples)
+    # Get samples of X on the original scale of each variable
+    x_samples =\
+        (x_standardized_samples *
+         standard_deviations[None, :, None] +
+         means[None, :, None])
+    return x_samples
