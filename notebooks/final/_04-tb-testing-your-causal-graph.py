14219ceeb3d6372755f35f4b504173439d5d2f66
Timothy Brathwaite <timothyb0912@gmail.com>
2020-06-02 22:23:04 -0700

    Adding public notebook skeleton.

---
+++ notebooks/final/_04-tb-testing-your-causal-graph.py
@@ -0,0 +1,56 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Testing your causal graph
+#
+# ## Purpose
+
+# ## Set notebook parameters
+
+
+
+# ## Import needed libraries
+
+
+
+# ## Load and describe needed data
+
+
+
+# ## Show the posited causal graph
+
+
+
+# ## Marginal independence tests
+
+
+
+# ## Conditional independence tests
+
+
+
+# ### Caveats and pitfalls
+
+
+
+# ## Latent conditional independence tests
+
+
+
+# ### Caveats and pitfalls
+
+
+
+# ## Summary
