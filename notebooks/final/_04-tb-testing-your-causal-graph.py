8696062a438027d7567fd924a6c1ca2bb612c900
Timothy Brathwaite <timothyb0912@gmail.com>
2020-06-08 22:41:40 -0700

    [WIP] Filled in marginal independence section. Started filling conditional independence section.

--- notebooks/final/_04-tb-testing-your-causal-graph.py
+++ notebooks/final/_04-tb-testing-your-causal-graph.py
@@ -43,6 +43,14 @@ UTILITY_COLUMNS =\
 
 # Note the travel mode of intersest for this notebook
 DRIVE_ALONE_ID = 1
+
+# Note the number of permutations to be used when
+# testing the causal graphs
+NUM_PERMUTATIONS = 100
+
+# Choose a color to represent reference /
+# permutation-based test statistics
+PERMUTED_COLOR = '#a6bddb'
 # -
 
 # ## Import needed libraries
@@ -93,14 +101,55 @@ causal_graph
 # ## Marginal independence tests
 #
 # ### Main idea
+# The marginal independence tests demonstrated in this notebook will visually test the following implication<br>
+# $
+# \begin{aligned}
+# P \left( X_1 \mid X_2 \right) &= P \left( X_1 \right) \\
+# \int x_1 P \left( X_1 \mid X_2 \right) \partial{x_1}  &= \int x_1 P \left( X_1 \right) \partial{x_1} \\
+# E \left[ X_1 \mid X_2 \right] &= E \left[ X_1 \right]
+# \end{aligned}
+# $
+#
+# In other words, if $X_1$ is marginally independent of $X_2$, then the expectation of $X_1$ conditional on $X_2$ is equal to the marginal expectation of $X_1$. Marginal independence implies mean independence. This means that shuffling / permuting the $X_2$ columns should make no difference to predicting $X_1$, once one predicts $\bar{X_1}$.
+#
+# The test demonstrated below works by estimating a linear regression to predict $E \left[ X_1 \mid X_2 \right]$. The $r^2$ from these regressions is computed using the observed value of $X_2$, and it is also computed using the permuted values of $X_2$ (which are independent of $X_1$ by construction).
+#
+# If $X_1$ is marginally independent of $X_2$, then the $r^2$ using the observed values of $X_2$ should be similar to the distribution of $r^2$ using the permuted values of $X_2$.
+#
+# For this test, we'll use the following marginal independence assumption implied by the causal graph above:<br>
+# $
+# P \left( \textrm{Number of Automobiles} \mid \textrm{Number of licensed drivers} \right) = P \left( \textrm{Number of Automobiles} \right)
+# $
 
+# +
+license_array = drive_alone_df[LICENSE_COLUMN].values
+num_cars_array = drive_alone_df[NUM_AUTOS_COLUMN].values
 
+oi.visual_permutation_test(
+    license_array,
+    num_cars_array,
+    z_array=None,
+    seed=1038,
+    num_permutations=NUM_PERMUTATIONS,
+    permutation_color=PERMUTED_COLOR)
+# -
 
 # ## Conditional independence tests
 #
 # ### Main idea
 
+# +
+time_array = drive_alone_df[TIME_COLUMN].values
+cost_array = drive_alone_df[COST_COLUMN].values
+distance_array = drive_alone_df[DISTANCE_COLUMN].values
 
+oi.visual_permutation_test(
+    time_array,
+    cost_array,
+    z_array=distance_array,
+    num_permutations=NUM_PERMUTATIONS,
+    permutation_color=PERMUTED_COLOR)
+# -
 
 # ### Caveats and pitfalls
 
