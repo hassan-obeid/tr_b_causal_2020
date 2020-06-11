f1dbf974a59e6fa7c56a3a07a8e13d5156b3e469
Timothy Brathwaite <timothyb0912@gmail.com>
2020-06-10 22:39:24 -0700

    Adding causal graph for latent confounder drive alone utility graph.

--- src/graphs/drive_alone_utility.py
+++ src/graphs/drive_alone_utility.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-A graphical model of the drive alone utility for our choice model.
+Graphical models of the drive alone utility for our choice model, assuming
+strong-ignorability (`DRIVE_ALONE_UTILITY`) and under the assumptions of
+unobserved confounding (`LATENT_DRIVE_ALONE_UTILITY`) from "The Blessings of
+Multiple Causes" (2018) by Wang and Blei.
 """
 from causalgraphicalmodels import CausalGraphicalModel
 
@@ -20,3 +23,13 @@ DRIVE_ALONE_UTILITY =\
                ("Number of Autos", "Utility (Drive Alone)"),
                ("Number of Licensed Drivers", "Utility (Drive Alone)")]
         )
+
+drive_alone_nodes = list(DRIVE_ALONE_UTILITY.dag.nodes)
+nodes_for_latent_graph = ['confounder'] + drive_alone_nodes
+edges_for_latent_graph =\
+    ([('confounder', x) for x in drive_alone_nodes] +
+     [(x, 'Utility (Drive Alone)') for x in drive_alone_nodes[:-1]])
+
+LATENT_DRIVE_ALONE_UTILITY =\
+    CausalGraphicalModel(nodes=nodes_for_latent_graph,
+                         edges=edges_for_latent_graph)
